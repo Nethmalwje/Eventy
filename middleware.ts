5df1fe20c64812ffe220a42183cbd5ff729a275f
Nethmal Wijesinghe <130041936+Nethmalwje@users.noreply.github.com>
2024-06-18 02:24:36 +0530

    first_commit

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
